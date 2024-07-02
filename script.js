d8c03864bbda621ac74cb6742e4b7c14b73646ac
Hugo060 <hugomorenogarcia1@gmail.com>
2024-07-02 18:00:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 function submitForm(event) {
     event.preventDefault();
     
-    let nombre = document.getElementById('nombre').value.trim(); // Trim para eliminar espacios en blanco al inicio y final
+    let nombre = document.getElementById('nombre').value;
     localStorage.setItem('nombre', nombre);
     
-    // Verificar si el nombre está en la lista de puntos
-    if (nombre in puntos) {
-        window.location.href = 'puntos.html';
-    } else {
-        mostrarError();
-    }
-}
-
-function mostrarError() {
-    let container = document.querySelector('.container');
-    let errorDiv = document.createElement('div');
-    errorDiv.classList.add('error');
-    errorDiv.textContent = 'Nombre incorrecto';
-    container.appendChild(errorDiv);
+    window.location.href = 'puntos.html';
 }
 
